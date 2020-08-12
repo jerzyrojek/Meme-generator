ef1d238906cb0df6bd477b77ff5240ec078a9f71
Jerzy Rojek <jerzyadamrojek@gmail.com>
2020-08-12 21:03:33 +0200

    generating inputs based on box count

--- App/js/Meme.js
+++ App/js/Meme.js
@@ -5,6 +5,9 @@ import SingleMeme from "./SingleMeme";
 const Meme = () => {
     const [memeTemplates, setMemeTemplates] = useState([]);
     const [chosenTemplate, setChosenTemplate] = useState(null);
+    const [chosenMemeIndex, setChosenMemeIndex] = useState(0);
+    const [captions, setCaptions] = useState([]);
+
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes").then(response => response.json().then(response => {
             const memeEmptyTemplates = response.data.memes;
@@ -12,17 +15,40 @@ const Meme = () => {
         }));
     }, []);
 
+    useEffect(() => {
+        if (memeTemplates.length) {
+            setCaptions(Array(memeTemplates[chosenMemeIndex].box_count).fill(""));
+        }
+    }, [chosenMemeIndex, memeTemplates]);
+
+
     return (
         <>
-            <h1 style={{textAlign:"center"}}>Choose a meme by clicking on it</h1>
-            {chosenTemplate && <SingleMeme templates={chosenTemplate}/>}
-            {!chosenTemplate && memeTemplates.map((memeTemplate) => {
+            <h1 style={{textAlign: "center"}}>Choose a meme by clicking on it</h1>
+            {chosenTemplate &&
+            <>
+                <SingleMeme templates={chosenTemplate}/>
+                {
+                        captions.map((caption, i) => (
+                            <input key={i}/>
+                        ))
+                }
+                <button onClick={() => (console.log("klik"))}>Generate</button>
+                <button onClick={() => {
+                    setChosenTemplate(null);
+                }
+                }>Back</button>
+            </>
+            }
+
+            {!chosenTemplate && memeTemplates.map((memeTemplate, i) => {
+
                 return (
-                   <SingleMeme key={memeTemplate.id} templates = {memeTemplate}
-                         onClick={() => {
-                             setChosenTemplate(memeTemplate);
-                         }}
-                         />
+                    <SingleMeme key={memeTemplate.id} templates={memeTemplate}
+                                onClick={() => {
+                                    setChosenMemeIndex(i);
+                                    setChosenTemplate(memeTemplate);
+                                }}/>
                 )
             })}
         </>
